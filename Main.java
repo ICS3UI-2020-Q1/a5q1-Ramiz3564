9957712e90f6e316b4dad5a83576dfd8f1b1ee70
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-10-06 13:46:00 +0000

    I finished practice Q1 Oct 6

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+ import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will create a multiplication table for whatever number user wants
+ * @author Zach Ramirez 
  */
 public class Main {
 
@@ -9,7 +10,19 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input 
+    Scanner input = new Scanner(System.in); 
+    //ask user for integer to create times table
+    System.out.println("please enter an integer to create a multiplication table for"); 
+    int integer = input.nextInt();  
+
+    //use while loop for multiplication 
+    for(int count = 1; count <= 12; count++){
+      int answer = count * integer; 
+      System.out.println(count + " x " + integer + " = " +answer); 
+    } 
+
+    
     
   }
 }
